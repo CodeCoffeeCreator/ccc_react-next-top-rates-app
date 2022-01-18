a70755b07f6c453fdad082a6a52dfcd0741d9577
Denny Pearce <digital.y.coffee@gmail.com>
2022-01-18 11:37:07 +0200

    add simple, but a few hardly than prev component -> Button (with svg icon, classnames etc)

--- components/index.ts
+++ components/index.ts
@@ -1,1 +1,2 @@
 export * from './HeaderTag/HeaderTag';
+export * from './Button/Button';
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import { HeaderTag, Button } from '../components';
+
 export default function Home(): JSX.Element {
-  return <div></div>;
+  return (
+    <>
+      <HeaderTag tag='h1'>Some texts</HeaderTag>
+      <Button appearance='primary' arrow='right'>
+        Button
+      </Button>
+      <Button appearance='ghost' arrow='down'>
+        Button
+      </Button>
+    </>
+  );
 }
