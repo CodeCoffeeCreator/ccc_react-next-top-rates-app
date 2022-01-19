bae64f5d0626834845ab537c714d1b223473a815
Denny Pearce <digital.y.coffee@gmail.com>
2022-01-19 22:15:29 +0200

    write styles for Layout, create full Footer

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,21 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
 export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+  return (
+    <div className={cn(className, styles.footer)} {...props}>
+      <div>
+        Top Rating Application © 2021 - {format(new Date(), 'yyyy')} All rights
+        protected
+      </div>
+      <a href='#' target='_blank'>
+        Terms of use
+      </a>
+      <a href='#' target='_blank'>
+        Private policy
+      </a>
+    </div>
+  );
 };
