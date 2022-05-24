ab52f86a996aeae878f653d313b2b1c5e47b022c
Denny Pearce <digital.y.coffee@gmail.com>
2022-05-24 13:47:13 +0300

    start write top page component and style it (just a few element for test)

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -1,9 +1,32 @@
+import { HeaderTag, Tag } from '../../components';
 import { TopPageComponentProps } from './TopPageComponent.props';
+import styles from './TopPageComponent.module.css';
 
 export const TopPageComponent = ({
   page,
   products,
   firstCategory,
 }: TopPageComponentProps): JSX.Element => {
-  return <>{products && products.length}</>;
+  return (
+    <>
+      <div className={styles.wrapper}>
+        <HeaderTag tag='h1'>{page.title}</HeaderTag>
+        {products && (
+          <Tag color='grey' size='m'>
+            {products.length}
+          </Tag>
+        )}
+        <span>Сортировка</span>
+      </div>
+      <div>
+        {products && products.map((p) => <div key={p._id}>{p.title}</div>)}
+      </div>
+      <div className={styles.hhTitle}>
+        <HeaderTag tag='h2'>Вакансии - {page.category}</HeaderTag>
+        <Tag color='red' size='m'>
+          hh.ru
+        </Tag>
+      </div>
+    </>
+  );
 };
